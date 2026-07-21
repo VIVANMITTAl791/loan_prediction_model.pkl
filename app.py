db84caabafe47a9f0c041ca983d3e7b8748f963e
VIVANMITTAl791 <175645107+VIVANMITTAl791@users.noreply.github.com>
2026-07-21 11:00:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -122,11 +122,10 @@ Enter the applicant's financial and personal details below to run the assessment
 
 developer_info = """
 ### About the Developer
-**Created by:** Chandan Saroj
+**Created by:** Vivan Mittal
 
-* **LinkedIn:** [Connect with me](YOUR_LINKEDIN_URL_HERE)
-* **GitHub:** [Check out my projects](YOUR_GITHUB_URL_HERE)
-* **Instagram:** [Follow me](YOUR_INSTAGRAM_URL_HERE)
+* **LinkedIn:** [Connect with me](www.linkedin.com/in/vivanmittal)
+
 
 ---
 ### 🛠️ Tools & Technologies Used
